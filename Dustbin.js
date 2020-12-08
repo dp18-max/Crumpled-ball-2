6d0ecbf7606671e065c31405a06968c397644f61
dp18-max <65004103+dp18-max@users.noreply.github.com>
2020-12-08 16:31:34 +0530

    Add files via upload

--- Dustbin.js
+++ Dustbin.js
@@ -8,6 +8,7 @@ class Dustbin{
         this.wallThickness = 20;
         this.angle=0;
 
+		this.image=loadImage("dustbingreen.png");
         this.bottomBody=Bodies.rectangle(this.x, this.y, this.dustbinWidth, this.wallThickness, {isStatic:true})
         this.leftWallBody=Bodies.rectangle(this.x-this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:true})
         Matter.Body.setAngle(this.leftWallBody, this.angle);
--- Paper.js
+++ Paper.js
@@ -9,6 +9,8 @@ class Paper{
         this.x=x;
         this.y=y;
         this.r=r;
+
+        this.image = loadImage("paper.png");
         this.body=Bodies.circle(this.x,this.y,this.r/2,options);
         World.add(world,this.body);
     }
